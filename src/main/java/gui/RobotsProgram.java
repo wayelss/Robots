df22ef19e17fc03c0cd1450691286ca85074210e
wayelss <cclooud44@gmail.com>
2026-03-07 15:24:55 +0500

    Добавил: 1)Добавил класс UI, в котором настраивается русский язык для приложения 2)Добавил класс WindowStateManager, который отвечает за состояние окон 3)Добавил меню для открытия окон 4)Отрефакторил старый код

--- src/main/java/gui/RobotsProgram.java
+++ src/main/java/gui/RobotsProgram.java
@@ -10,10 +10,6 @@ public class RobotsProgram
 {
     public static void main(String[] args) {
       try {
-        //System.out.println(Locale.getDefault());
-//        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-//        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
       } catch (Exception e) {
         e.printStackTrace();
       }
